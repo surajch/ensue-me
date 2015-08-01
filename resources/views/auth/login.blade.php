ada2bb0190303402e6ed92fcb9c50256f1c81cba
suham <suham@Saidul.com>
2015-08-01 19:30:37 +0530

    Initial push

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,9 @@
-@extends('app')
+@extends('layouts.dashboard')
 
 @section('content')
 <div class="container-fluid">
 	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
+		<div class="col-md-6 col-md-offset-3">
 			<div class="panel panel-default">
 				<div class="panel-heading">Login</div>
 				<div class="panel-body">
@@ -18,7 +18,7 @@
 						</div>
 					@endif
 
-					<form class="form-horizontal" role="form" method="POST" action="{{ asset('/auth/login') }}">
+					<form class="form-horizontal" role="form" method="POST" action="/auth/login">
 						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
 						<div class="form-group">
---
+++ resources/views/dashboard/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.dashboard')
+
+@yield('body')
+
+
+@stop
