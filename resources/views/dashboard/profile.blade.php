f9272fe8c71f9cf82454317959e771d3a22a1e33
Monsoor <monsoor.zontro@gmail.com>
2015-08-03 23:13:34 +0530

    we created some layout

--- resources/views/dashboard/profile.blade.php
+++ resources/views/dashboard/profile.blade.php
@@ -32,10 +32,9 @@
 
 	</div>
 
+
 	<!-- edit form column -->
 	<div class="col-md-9 personal-info">
-		
-		<h3>Personal info</h3>
 
 		{{ Form::open([
 			'url' => '/profile/'.$user->id,
@@ -45,39 +44,51 @@
 			]) 
 		}}
 
-			<div class="form-group">
-				<label class="col-lg-3 control-label">Name:</label>
-				<div class="col-lg-8">
-					<input class="form-control" name="name" type="text" value="{{ $user->name }}">
+
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					Personal Information
 				</div>
+				  <div class="panel-body">
+						<div class="form-group">
+						<label class="col-lg-3 control-label">Name:</label>
+						<div class="col-lg-8">
+							<input class="form-control" name="name" type="text" value="{{ $user->name }}">
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="col-lg-3 control-label">Email:</label>
+						<div class="col-lg-8">
+							<input class="form-control" name="email" type="text" value="{{ $user->email }}">
+						</div>
+					</div>
+					<!-- <div class="form-group">
+						<label class="col-md-3 control-label">Password:</label>
+						<div class="col-md-8">
+							<input class="form-control" type="password" value="">
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="col-md-3 control-label">Confirm password:</label>
+						<div class="col-md-8">
+							<input class="form-control" type="password" value="">
+						</div>
+					</div>-->
+					
+				  </div>
+				  <div class="panel-footer">
+						<div class="form-group">
+							<label class="col-md-3 control-label"></label>
+							<div class="col-md-8 text-right">
+								<input type="submit" class="btn btn-primary" value="Save Changes" />
+								<span></span>
+								<a href="{{ url('/home') }}" class="btn btn-danger" >Cancel
+								</a>
+							</div>
+						</div>  
+					</div>
 			</div>
-			<div class="form-group">
-				<label class="col-lg-3 control-label">Email:</label>
-				<div class="col-lg-8">
-					<input class="form-control" name="email" type="text" value="{{ $user->email }}">
-				</div>
-			</div>
-			<!-- <div class="form-group">
-				<label class="col-md-3 control-label">Password:</label>
-				<div class="col-md-8">
-					<input class="form-control" type="password" value="">
-				</div>
-			</div>
-			<div class="form-group">
-				<label class="col-md-3 control-label">Confirm password:</label>
-				<div class="col-md-8">
-					<input class="form-control" type="password" value="">
-				</div>
-			</div>-->
-			<div class="form-group">
-				<label class="col-md-3 control-label"></label>
-				<div class="col-md-8">
-					<input type="submit" class="btn btn-primary" value="Save Changes" />
-					<span></span>
-					<a href="{{ url('/home') }}" class="btn btn-danger" >Cancel
-					</a>
-				</div>
-			</div> 
+			
 		{{ Form::close() }}
 	</div>
 </div>
