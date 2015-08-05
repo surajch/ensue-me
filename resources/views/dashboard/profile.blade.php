26688dcc1b2b94ae3e180d9a985fd4f5c4286830
deepakonrid <deepak.zontro@gmail.com>
2015-08-05 21:05:09 +0530

    User Profile Table

--- resources/views/dashboard/profile.blade.php
+++ resources/views/dashboard/profile.blade.php
@@ -47,19 +47,17 @@
 
   <!-- Nav tabs -->
   <ul class="nav nav-tabs" role="tablist">
-    <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
-    <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
-    <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Messages</a></li>
-    <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Settings</a></li>
+    <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Personal Information</a></li>
+    <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Common Settings</a></li>
+    <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Specific Settings</a></li>
+    <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Messages</a></li>
   </ul>
 
   <!-- Tab panes -->
   <div class="tab-content">
-    <div role="tabpanel" class="tab-pane active" id="home">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
-    <div role="tabpanel" class="tab-pane" id="profile">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</div>
-    <div role="tabpanel" class="tab-pane" id="messages"><div class="panel panel-default">
+    <div role="tabpanel" class="tab-pane active" id="home"><div class="panel panel-default">
 				<div class="panel-heading">
-					Personal Information
+				User Information
 				</div>
 				  <div class="panel-body">
 						<div class="form-group">
@@ -100,9 +98,10 @@
 						</div>  
 					</div>
 			</div></div>
-    <div role="tabpanel" class="tab-pane" id="settings"><div class="panel panel-default">
+    <div role="tabpanel" class="tab-pane" id="profile">
+    <div class="panel panel-default">
 				<div class="panel-heading">
-					Security
+					Privacy
 				</div>
 				  <div class="panel-body">
 						<div class="form-group">
@@ -143,9 +142,63 @@
 						</div>  
 					</div>
 			</div></div>
-  </div>
+    <div role="tabpanel" class="tab-pane" id="messages">
+    <div class="panel panel-default">
+				<div class="panel-heading">
+				Security	
+				</div>
+				  <div class="panel-body">
+						<div class="form-group">
+						<label class="col-lg-3 control-label">Name:</label>
+						<div class="col-lg-8">
+							<input class="form-control" name="name" type="text" value="{{ $user->name }}">
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="col-lg-3 control-label">Email:</label>
+						<div class="col-lg-8">
+							<input class="form-control" name="email" type="text" value="{{ $user->email }}">
+						</div>
+					</div>
+					<!-- <div class="form-group">
+						<label class="col-md-3 control-label">Password:</label>
+						<div class="col-md-8">
+							<input class="form-control" type="password" value="">
+						</div>
+					</div>
+					<div class="form-group">
+						<label class="col-md-3 control-label">Confirm password:</label>
+						<div class="col-md-8">
+							<input class="form-control" type="password" value="">
+						</div>
+					</div>-->
+					
+				  </div>
+				  <div class="panel-footer">
+						<div class="form-group">
+							<label class="col-md-3 control-label"></label>
+							<div class="col-md-8 text-right">
+								<input type="submit" class="btn btn-primary" value="Save Changes" />
+								<span></span>
+								<a href="{{ url('/home') }}" class="btn btn-danger" >Cancel
+								</a>
+							</div>
+						</div>  
+					</div>
+			</div></div>
+    <div role="tabpanel" class="tab-pane" id="settings">
+   		<h3 style="text-align: justify;"><a href="#">Life Messages</a></h3>
+  		 <p style="text-align: justify;">God never promised a happy and easy life but he did promise that you can bring a beautiful change to it by your consistent efforts. Life is beautiful but there are some terms and conditions attached with it. It is a journey and not a destination, so make sure that you enjoy every moment to its optimum levels so that there are no regrets later. Every life has a purpose and discovering that purpose should be the goal of life because that is the gift of God.</p>
+   		<p style="text-align: justify;">Here are some of sweet life messages examples for different situations and relations which can help you add new energy to the lives of the people around you.</p>
+   		<h3 style="text-align: justify;"><a href="#">Life Messages for Friends</a></h3>
+   		<p style="text-align: justify;">Friends are life. And sometimes when your dear friend feels rejected from life and feels completely discourages then you must explain him the importance and wonderful aspects of life which he still needs to explore and enjoy.</p>
+   		<blockquote>
+		<p style="text-align: justify;"><span style="color: #333333;">“Life never pleases us by giving us what we want but it always surprises us with things that we deserve. So never underestimate yourself as you deserve the best.”</span></p>
+</blockquote>
 
 </div>
+
+
 
 
 			
