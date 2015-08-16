ccc338b3c4062372772445fce41e794675da42d5
Monsoor <monsoor.zontro@gmail.com>
2015-08-16 15:39:10 +0530

    Migration user changed

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,10 @@ class CreateUsersTable extends Migration {
 		Schema::create('users', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('name');
-			$table->string('email')->unique();
-			$table->string('password', 60);
+			$table->string('name', 255);
+			$table->string('email', 255)->unique();
+			$table->string('password', 255);
+			$table->integer('role_id');
 			$table->rememberToken();
 			$table->timestamps();
 		});
