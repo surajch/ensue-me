8cead147f55c5ffe801fc9744b221dd8923f9176
Suraj <suraj.zontro@gmail.com>
2015-08-23 22:20:05 +0530

    Routes modified

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,6 +14,7 @@
 Route::get('/', 'WelcomeController@index');
 Route::get('/test', 'TestController@index');
 Route::get('/home', 'HomeController@index');
+
 Route::get('facebook', 'AccountController@facebook_redirect');
 Route::get('account/facebook', 'AccountController@facebook');
 
